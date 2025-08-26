9828fa8b96289794d16119e0835b9be39aec8b4b
Alexander Stolnikov <stolnikov-a@mail.ru>
2025-08-26 14:03:16 +0300

    HW_7

--- HW7/D09.c
+++ HW7/D09.c
@@ -1,23 +1,21 @@
-//  Дано натуральное число n ≥ 1. Проверьте, является ли оно простым.
-//  Программа должна вывести слово YES, если число простое или NO
-//  в противном случае. Необходимо составить рекурсивную функцию
-//  и использовать ее. int is_prime(int n, int delitel)
+//  Дано натуральное число N. Вычислите сумму его цифр.
+//  Необходимо составить рекурсивную функцию.
+//  int sum_digits(int n)
 
 #include <stdio.h>
 
-int is_prime(int n, int delitel)
+int sum_digits(int n) 
 {
-    if (delitel == 1)
-        return 1;
-    else if (n % delitel == 0)
+    if(n <= 0) 
         return 0;
-    else return is_prime(n, delitel--);
+    int sum = (n % 10) + sum_digits(n/10);
+    return sum;
 }
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
-    is_prime(n, n/2) ? printf("YES") : printf("NO");
+    printf("%d ", sum_digits(n));
     return 0;
 }
